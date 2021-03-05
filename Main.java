cfec37a19084fa1650ebbee9b61ccb7b5144a744
Mason Baesel <mason.baesel@stu.boone.kyschools.us>
2021-03-05 13:39:13 +0000

    code

--- Main.java
+++ Main.java
@@ -7,7 +7,25 @@ class Main
 {
   public static void main(String[] args) 
   {
-
-
+    String adj = JOptionPane.showInputDialog("Tell me a adjetive");
+    String adjetive = JOptionPane.showInputDialog("Yell me a adjetive");
+    String bird = JOptionPane.showInputDialog("Tell me a type of bird")
+    String room = JOptionPane.showInputDialog("tell me a room in your house")
+    String Verb = JOptionPane.showInputDialog("Tell me a verb in past tense")
+    String verb = JOptionPane.showInputDialog("tell me a verb")
+    String name = JOptionPane.showInputDialog("tell me a relatives name")
+    String noun = JOptionPane.showInputDialog("tell me a noun")
+    String liquid = JOptionPane.showInputDialog("tell me a liquid")
+    String Verbing = JOptionPane.showInputDialog("tell me a verb ending in ing")
+    System.out.println("It was a "+adj);
+    System.out.println("cold November day. I woke up to the"+adjetive);
+    System.out.println("smell of"+bird)
+    System.out.println("roasting in the"+room)
+    System.out.println("downstairs. I"+Verb)
+    System.out.println("down the stairs to see if I could help"+verb)
+    System.out.println("the dinner. My mom said see if "+name)
+    System.out.println("needs a fresh"+noun)
+    System.out.println("So I carried a tray of glasses full of"+liquid)
+    System.out.println("into the"+Verbing+"room")
   }
 }
